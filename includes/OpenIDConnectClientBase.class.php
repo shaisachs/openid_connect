c1081b0b7e00a9e9623a90eb6d15bea3c8654ab7
shaisachs <ssachs@ngpvan.com>
2015-02-04 16:09:11 -0500

    Backport includes/ to Drupal 6
    Signed-off-by: shaisachs <ssachs@ngpvan.com>

--- includes/OpenIDConnectClientBase.class.php
+++ includes/OpenIDConnectClientBase.class.php
@@ -91,19 +91,19 @@ abstract class OpenIDConnectClientBase implements OpenIDConnectClientInterface {
    */
   public function authorize($scope = 'openid email') {
     $redirect_uri = OPENID_CONNECT_REDIRECT_PATH_BASE . '/' . $this->name;
-    $url_options = array(
-      'query' => array(
-        'client_id' => $this->getSetting('client_id'),
-        'response_type' => 'code',
-        'scope' => $scope,
-        'redirect_uri' => url($redirect_uri, array('absolute' => TRUE)),
-        'state' => openid_connect_create_state_token(),
-      ),
+
+    $query = array(
+      'client_id' => $this->getSetting('client_id'),
+      'response_type' => 'code',
+      'scope' => $scope,
+      'redirect_uri' => url($redirect_uri, array('absolute' => TRUE)),
+      'state' => openid_connect_create_state_token(),
     );
+    
     $endpoints = $this->getEndpoints();
     // Clear $_GET['destination'] because we need to override it.
     unset($_GET['destination']);
-    drupal_goto($endpoints['authorization'], $url_options);
+    drupal_goto($endpoints['authorization'], $query);
   }
 
   /**
@@ -119,16 +119,15 @@ abstract class OpenIDConnectClientBase implements OpenIDConnectClientInterface {
       'redirect_uri' => url($redirect_uri, array('absolute' => TRUE)),
       'grant_type' => 'authorization_code',
     );
-    $request_options = array(
-      'method' => 'POST',
-      'data' => drupal_http_build_query($post_data),
-      'timeout' => 15,
-      'headers' => array('Content-Type' => 'application/x-www-form-urlencoded'),
-    );
+
+    $headers = array('Content-Type' => 'application/x-www-form-urlencoded');
+    $data = $this->drupal_http_build_query($post_data);
     $endpoints = $this->getEndpoints();
-    $response = drupal_http_request($endpoints['token'], $request_options);
+    $response = drupal_http_request($endpoints['token'], $headers, 'POST',
+      $data, 3, 15);
+
     if (!isset($response->error) && $response->code == 200) {
-      $response_data = drupal_json_decode($response->data);
+      $response_data = json_decode($response->data, TRUE);
       return array(
         'id_token' => $response_data['id_token'],
         'access_token' => $response_data['access_token'],
@@ -148,26 +147,52 @@ abstract class OpenIDConnectClientBase implements OpenIDConnectClientInterface {
     list($headerb64, $claims64, $signatureb64) = explode('.', $id_token);
     $claims64 = str_replace(array('-', '_'), array('+', '/'), $claims64);
     $claims64 = base64_decode($claims64);
-    return drupal_json_decode($claims64);
+    return json_decode($claims64, TRUE);
   }
 
   /**
    * Implements OpenIDConnectClientInterface::retrieveUserInfo().
    */
   public function retrieveUserInfo($access_token) {
-    $request_options = array(
-      'headers' => array(
-        'Authorization' => 'Bearer ' . $access_token,
-      ),
+    $headers = array(
+      'Authorization' => 'Bearer ' . $access_token,
     );
     $endpoints = $this->getEndpoints();
-    $response = drupal_http_request($endpoints['userinfo'], $request_options);
+    $response = drupal_http_request($endpoints['userinfo'], $headers);
     if (!isset($response->error) && $response->code == 200) {
-      return drupal_json_decode($response->data);
+      return json_decode($response->data, TRUE);
     }
     else {
       openid_connect_log_request_error(__FUNCTION__, $this->name, $response);
       return FALSE;
     }
   }
+
+  /**
+   * Drupal 6 backport of
+   * https://api.drupal.org/api/drupal/includes!common.inc/function/drupal_http_build_query/7
+   */
+  protected function drupal_http_build_query(array $query, $parent = '') {
+    $params = array();
+
+    foreach ($query as $key => $value) {
+      $key = ($parent ? $parent . '[' . rawurlencode($key) . ']' : rawurlencode($key));
+
+      // Recurse into children.
+      if (is_array($value)) {
+        $params[] = $this->drupal_http_build_query($value, $key);
+      }
+      // If a query parameter value is NULL, only append its key.
+      elseif (!isset($value)) {
+        $params[] = $key;
+      }
+      else {
+        // For better readability of paths in query strings, we decode slashes.
+        $params[] = $key . '=' . str_replace('%2F', '/', rawurlencode($value));
+      }
+    }
+
+    return implode('&', $params);
+  }
+
 }
